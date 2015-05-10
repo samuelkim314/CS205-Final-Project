93a522e6f2a77325045fc18065b0e02c25588a35
Robert Hoyt <rhoyt@fas.harvard.edu>
2015-05-10 16:09:10 -0400

    added print statements to driver.py

--- driver.py
+++ driver.py
@@ -1,14 +1,14 @@
-from sklearn.tree import DecisionTreeClassifier as Tree
+#from sklearn.tree import DecisionTreeClassifier as Tree
 from sklearn.ensemble import RandomForestClassifier as Forest
-from sklearn.ensemble import ExtraTreesClassifier as EForest
-from sklearn.cross_validation import train_test_split as sk_split
-from sklearn.neighbors import KNeighborsClassifier as KNN
+#from sklearn.ensemble import ExtraTreesClassifier as EForest
+#from sklearn.cross_validation import train_test_split as sk_split
+#from sklearn.neighbors import KNeighborsClassifier as KNN
 import generate_GPS as gGPS
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import time
-import sys
+#import matplotlib.pyplot as plt
+#import time
+#import sys
 import re
 import preprocess as pp
 
@@ -20,6 +20,7 @@ middle_mrmr = False # Use MRMR to eliminate features early for speed
    
 if __name__ == '__main__':
     #Data import 
+    print "Loading data..."
     train_data = pd.read_csv('train.csv')
     train_labels = pd.read_csv('train_label.csv')
     compete_data = pd.read_csv('test.csv')
@@ -35,12 +36,13 @@ if __name__ == '__main__':
     #Make a pd.Series with training labels
     label = train_labels.status_group
     
+    print "Preprocessing numerical features"
 	#Preprocess the data and add rotated GPS coords to it
     for frame in [td, cd]:
         pp.initial_preprocess(frame)
-        added_num_feat = pp.process_GPS(frame)
+# RAH FIXME: Redundant with gGPS.generate_GPS?        added_num_feat = pp.process_GPS(frame)
    
-    numer_feat += added_num_feat
+#    numer_feat += added_num_feat
     
     #Vectorize the target variables    
     target = np.zeros(td.shape[0],dtype=np.int)
@@ -48,6 +50,7 @@ if __name__ == '__main__':
     target[np.array(label=='functional needs repair')] = 1
     target[np.array(label=='functional')] = 2
 
+    print "Preprocessing GPS data"
     added_features = gGPS.generate_GPS(td, target, cd)
     numer_feat += added_features
 
@@ -69,6 +72,7 @@ if __name__ == '__main__':
     irr_out = pp.filter_irrelevant_values(td, cd, numer_feat, label, p_mi)
       
     if middle_mrmr:
+        print "middle_mrmr: Eliminating features"
         #Now go through the remaining categorical features and retain only those which deliver 
         #large information gains and low redundance within the set
         mrmr_feat, mrmr_scores = pp.get_MRMR_features(td, numer_feat, label)
@@ -78,10 +82,12 @@ if __name__ == '__main__':
         #The MRMR for entire features tends to eliminate a lot of features
         mrmr_feat = list( set(td.columns) - set(numer_feat))
 	
+    print "One-hot encoding remaining features"
     #Vectorize remaining features
     new_cfl = pp.one_hot(td, mrmr_feat)
     pp.one_hot(cd, mrmr_feat)
     
+    print "Eliminating remaining features"
     #Now go through the vectorized variables one more time
     hot_mrmr_feat, hot_mrmr_scores = pp.get_MRMR_features(td, numer_feat, label)
     pp.filter_MRMR_features(td, numer_feat, hot_mrmr_feat)
@@ -94,6 +100,7 @@ if __name__ == '__main__':
     data_matrix = td.as_matrix().astype(np.float);
     data_matrix_compete = cd.as_matrix().astype(np.float);
     
+    print "Exporting remaining features"
     data_matrix.dump('processed_train_data')	
     data_matrix_compete.dump('processed_compete_data')
     target.dump('processed_targets')
@@ -101,12 +108,11 @@ if __name__ == '__main__':
 
     ##End of data preprocessing - next is forest creation and fitting
      
-     
     #Here you can use numpy.load on the filenames specified before
     #Set up a forest predictor
     forest = Forest(n_estimators=200, criterion='gini', n_jobs=4, \
                     verbose=True, max_features='auto', bootstrap=True, \
-                    min_samples_split=7, min_samples_leaf=1, oob_score=True)
+                    min_samples_split=1, min_samples_leaf=1, oob_score=True)
 
     forest.fit(data_matrix,target)
 	
